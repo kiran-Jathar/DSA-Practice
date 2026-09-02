73dfd4baa84f97cdda0174eb876016ceb306f551
Kiran Jathar <169063370+kiran-Jathar@users.noreply.github.com>
2026-09-03 00:36:45 +0530

    Time: 7 ms (6.22%), Space: 47.6 MB (68.89%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int i=0, e=s.length()-1;
-        //Check for first pointer where characters are not equal and break the loop
+                        
         while(i<e) {
             if(s.charAt(i)==s.charAt(e)) {
                 i++;
@@ -11,15 +11,16 @@ class Solution {
             }
         }
         if(i>=e) {
-            return true; //If you found no point of error just return true
+            return true; 
         }
-        if(!Palindrome(i,e-1, s)) { //Check again palindrome by removing character at end
-            return Palindrome(i+1,e, s); //If it fails check palindrome by removing character at start
+        if(!Palindrome(i,e-1, s)) { 
+            return Palindrome(i+1,e, s); 
         }
-        return true; //If removing from end works just return true
+        return true; 
     }
-    // Function for checking palindrome -
+    
     private boolean Palindrome(int i, int e, String s) {
+
         while(i<e) {
             if(s.charAt(i)==s.charAt(e)) {
                 i++;
@@ -31,3 +32,5 @@ class Solution {
         return true;
     }
 }
+ 
+
