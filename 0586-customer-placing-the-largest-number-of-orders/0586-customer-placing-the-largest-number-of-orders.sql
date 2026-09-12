cf9720446ec9d751ca11c5432004510c34beeada
Kiran Jathar <169063370+kiran-Jathar@users.noreply.github.com>
2026-09-13 00:37:55 +0530

    Time: 562 ms (32.44%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-
-# asa couster jyane largest number of order place kelet
-
-SELECT customer_number FROM Orders
-GROUP BY customer_number 
-ORDER BY COUNT(customer_number) DESC
+select customer_number 
+from orders
+GROUP BY customer_number
+ORDER BY count(*) DESc 
 LIMIT 1;
